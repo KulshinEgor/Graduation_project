32b37155c6073f1cabb80ee24c1a13e679971dce
egorkulsin <egor.kulshin@yandex.ru>
2023-10-19 13:52:27 +0300

    !

---
+++ SQL.py
@@ -0,0 +1,16 @@
+SELECT COUNT (o.id),
+                c.login
+FROM “Couriers” AS c
+INNER JOIN “Orders” AS o ON c.id=o.“courierId”
+WHERE o.“inDelivery” = true;
+GROUP BY c.login
+
+
+SELECT track,
+               CASE
+                         WHEN finished == true THEN 2
+                         WHEN canсelled == true THEN -1
+                         WHEN “inDelivery” == true THEN 1
+                         ELSE 0
+              END AS status
+FROM “Orders”;
